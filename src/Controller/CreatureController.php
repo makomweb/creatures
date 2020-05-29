914ccb2f44221751aec2f1a393d60eedcc1fc575
Martin Komischke <martin.komischke@outlook.com>
2020-05-29 11:09:24 +0200

    add logic to persist a new Creature entity

--- src/Controller/CreatureController.php
+++ src/Controller/CreatureController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Creature;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +16,28 @@ class CreatureController extends AbstractController
         return $this->render('creature/index.html.twig', [
             'controller_name' => 'CreatureController',
         ]);
+    }
+
+    /**
+     * @Route("/creature", name="create_creature")
+     */
+    public function createProduct(): Response
+    {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $creature = new Creature();
+        $creature->setName('Goblin');
+        $creature->setAttack(1999);
+        $creature->setDefense(222);
+
+        // tell Doctrine you want to (eventually) save the Creature (no queries yet)
+        $entityManager->persist($creature);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new creature with id '.$creature->getId());
     }
 }
