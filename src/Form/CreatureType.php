8d7297418cc494dce4fdc1e059d52e87c7ba7c7c
Martin Komischke <martin.komischke@outlook.com>
2020-06-02 19:05:15 +0200

    continue with form

--- src/Form/CreatureType.php
+++ src/Form/CreatureType.php
@@ -6,15 +6,27 @@ use App\Entity\Creature;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CreatureType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder        
-            ->add('Name')
+            ->add('Name', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter the name here',
+                    'class' => 'custom'
+                ]
+            ])
             ->add('Attack')
             ->add('Defense')
+            ->add('Save', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
         ;
     }
 
